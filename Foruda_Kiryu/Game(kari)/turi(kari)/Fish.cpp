ca9595aa8b4f29bbe3ae1f18891ddc64767345fb
IzumidaKiryu <ca01244003@st.kawahara.ac.jp>
2025-03-13 15:58:06 +0900

    Revert "Merge pull request #19 from IzumidaKiryu/K2Engine_Play"
    This reverts commit a15812fe84f59512d611a8c540b7585fd0e46c41, reversing
    changes made to 84734b7c87a2a4217665ab34936ddca5b8639966.

--- Foruda_Kiryu/Game(kari)/turi(kari)/Fish.cpp
+++ Foruda_Kiryu/Game(kari)/turi(kari)/Fish.cpp
@@ -15,24 +15,5 @@ void Fish::Update()
 
 void Fish::SetIndividualValue()
 {
-	m_individualValue = individualValue;
-}
-
-void Fish::SetTimeUntilEscape(float timeUntilEscape)//“¦‚°‚é‚Ü‚Å‚ÌŠÔ‚ğİ’è‚·‚éB
-{
-	m_timeUntilEscape = timeUntilEscape;
-}
-
-bool Fish::TimeCount()
-{
-	/*if (m_time < m_timeUntilEscape) {*/
-		m_time++;
-		if (m_time >= m_timeUntilEscape)
-		{
-			return true;
-		}
-		else {
-			return false;
-		}
-	//}
+	
 }
--- Foruda_Kiryu/Game(kari)/turi(kari)/PlayFishing.cpp
+++ Foruda_Kiryu/Game(kari)/turi(kari)/PlayFishing.cpp
@@ -1,25 +1,2 @@
 #include "stdafx.h"
 #include "PlayFishing.h"
-#include"PlayFishingBackGround.h"
-#include "GameCamera.h"
-#include "CastGauge.h"
-
-PlayFishing::PlayFishing()
-{
-	m_playFishingBackGround = NewGO< PlayFishingBackGround>(0, "playFishingBackGround");
-	CallCastGauge();
-
-}
-
-PlayFishing::~PlayFishing()
-{
-}
-
-void PlayFishing::Update()
-{
-}
-
-void PlayFishing::CallCastGauge()
-{
-	castGauge=NewGO< CastGauge>(0, "CastGauge");
-}
