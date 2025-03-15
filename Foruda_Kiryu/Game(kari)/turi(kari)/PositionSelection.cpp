9195e1e7a76a9a2795929d847ea7441aa212eb1f
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-03-15 13:48:55 +0900

    ポジションセレクションクラスにUIの実装

--- Foruda_Kiryu/Game(kari)/turi(kari)/PositionSelection.cpp
+++ Foruda_Kiryu/Game(kari)/turi(kari)/PositionSelection.cpp
@@ -24,6 +24,13 @@ PositionSelection::PositionSelection()
 	/*gameBGM->Init(1);
 	gameBGM->Play(true);*/
 	gameCamera->m_toCameraPos.Set(0.0f, 1250.0f, -250.0f);
+
+	//UIの場所を決める。
+	SetFishDisplayPosition();
+
+	//UIを設定する。
+	SetUI();
+
 }
 
 PositionSelection::~PositionSelection()
@@ -44,9 +51,53 @@ void PositionSelection::Update()
 
 void PositionSelection::Render(RenderContext& rc)
 {
+	for (int i = 0; i < 6; i++) {
+		m_fishDisplayInside[i].Draw(rc);
+		m_fishDisplayOutside[i].Draw(rc);
+	}
 }
 
-void PositionSelection::fishingPositionA_SetFish()
+void PositionSelection::SettingFishType(fishingPosition Position)
 {
-
 }
+
+void PositionSelection::SelectChangeFish(fishingPosition Position)
+{
+}
+
+void PositionSelection::SetUI()
+{
+	for (int i = 0; i < 6; i++) {
+		//魚を表示するディスプレイの内側
+		m_fishDisplayInside[i].Init("Assets/modelData/fish_display_ui_inside.DDS", 150, 150);
+		m_fishDisplayInside[i].SetPivot(Vector2(0.5f, 0.5f));
+		m_fishDisplayInside[i].SetPosition(m_fishDisplayPosition[i]);
+		m_fishDisplayInside[i].SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
+		m_fishDisplayInside[i].Update();
+
+		//魚を表示するディスプレイの外側
+		m_fishDisplayOutside[i].Init("Assets/modelData/fish_display_ui_outside.DDS", 150, 150);
+		m_fishDisplayOutside[i].SetPivot(Vector2(0.5f, 0.5f));
+		m_fishDisplayOutside[i].SetPosition(m_fishDisplayPosition[i]);
+		m_fishDisplayOutside[i].SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
+		m_fishDisplayOutside[i].Update();
+
+	}
+}
+
+void PositionSelection::SetFishDisplayPosition()
+{
+	for (int i = 0; i < 6; i++) {
+		if (i < 3) {
+			m_fishDisplayPosition[i] = { m_fishDisplayPositionXcriteria+m_fishDisplayPositionXinterval*(i), 200.0f, 0.0f };
+		}
+		if (2 < i) {
+			m_fishDisplayPosition[i] = { m_fishDisplayPositionXcriteria + m_fishDisplayPositionXinterval * (i-3), -200.0f, 0.0f };
+		}
+	}
+}
+
+//void PositionSelection::fishingPositionA_SetFish()
+//{
+//
+//}
--- Foruda_Kiryu/Game(kari)/turi(kari)/PositionSelection.h
+++ Foruda_Kiryu/Game(kari)/turi(kari)/PositionSelection.h
@@ -31,12 +31,22 @@ public:
 	void Render(RenderContext& rc);
 	void SettingFishType(fishingPosition Position);//魚のタイプを設定。
 	void SelectChangeFish(fishingPosition Position);//魚を変えるかどうか選ぶ。
+	void SetUI();
+	void SetFishDisplayPosition();
+
 	float setFish=0.0f;
+	Vector3 m_fishDisplayPosition[6];
+	float m_fishDisplayPositionXcriteria = -300;//魚を表示するディスプレイの場所の基準（X軸）
+	float m_fishDisplayPositionXinterval = 300.0f;
+
+
 	Player* player;			//プレイヤー。
 	GameCamera* gameCamera;			//ゲームカメラ。
 	BackGround* backGround;
 	SoundSource* gameBGM;		//ゲーム中のBGM。
 	FontRender m_fontRender;
+	SpriteRender m_fishDisplayInside[6];
+	SpriteRender m_fishDisplayOutside[6];
 	fishingPosition PositionA;
 	fishingPosition PositionB;
 	fishingPosition PositionC;
