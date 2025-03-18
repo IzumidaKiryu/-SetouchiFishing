97256bc35fc494de42373787f37371297f435894
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-03-18 18:54:56 +0900

    フィッシングマネージャークラスの魚を選んでNewGOする処理の実装

--- Foruda_Kiryu/Game(kari)/turi(kari)/FishManager.h
+++ Foruda_Kiryu/Game(kari)/turi(kari)/FishManager.h
@@ -1,5 +1,30 @@
 #pragma once
-class FishManager
+enum FishType {
+	TAI,
+	BURI,
+	TATIUO,
+	HIRAME,
+	JAKOTENN,
+	SINJU,
+};
+
+class Buri;
+class Tatiuo;
+class FishManager :IGameObject
 {
+public:
+
+	FishManager();
+	~FishManager();
+	void Update();
+	void SelectFishType();
+	void NewGOBuri();
+	void NewGOTatiuo();
+	void GetUI();
+	void FishNewGO();
+	FishType m_fishType;
+	Buri* m_buri;
+	Tatiuo* m_tatiuo;
+	SpriteRender* m_ui;
 };
 
