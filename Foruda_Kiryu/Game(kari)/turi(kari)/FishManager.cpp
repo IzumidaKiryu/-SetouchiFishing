ba7ffe8c9ce3b622bd2738a1603d208490bb1e17
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-03-18 18:54:56 +0900

    フィッシングマネージャークラスの魚を選んでNewGOする処理の実装

--- Foruda_Kiryu/Game(kari)/turi(kari)/FishManager.cpp
+++ Foruda_Kiryu/Game(kari)/turi(kari)/FishManager.cpp
@@ -1,2 +1,87 @@
 #include "stdafx.h"
 #include "FishManager.h"
+#include "Buri.h"
+#include "Tatiuo.h"
+
+FishManager::FishManager()
+{
+	srand(time(NULL));
+	SelectFishType();
+	FishNewGO();
+}
+
+FishManager::~FishManager()
+{
+
+}
+
+void FishManager::Update()
+{
+}
+
+void FishManager::SelectFishType()
+{
+	int randum=rand()/100;
+	if (0 <= randum <= 49) {
+		m_fishType = BURI;
+	}
+	if (50 <= randum <= 100) {
+		m_fishType = TATIUO;
+	}
+}
+
+void FishManager::NewGOBuri()
+{
+	m_buri = NewGO<Buri>(0, "buri");
+}
+
+void FishManager::NewGOTatiuo()
+{
+	m_tatiuo = NewGO<Tatiuo>(0, "tatiuo");
+}
+
+void FishManager::GetUI()
+{
+	switch (m_fishType)
+	{
+	case TAI:
+		break;
+	case BURI:
+		m_ui = m_buri->m_ui;
+		break;
+	case TATIUO:
+		m_ui = m_tatiuo->m_ui;
+		break;
+	case HIRAME:
+		break;
+	case JAKOTENN:
+		break;
+	case SINJU:
+		break;
+	default:
+		break;
+	}
+}
+
+void FishManager::FishNewGO()
+{
+	switch (m_fishType)
+	{
+	case TAI:
+		break;
+	case BURI:
+		NewGOBuri();
+		break;
+	case TATIUO:
+		NewGOTatiuo();
+		break;
+	case HIRAME:
+		break;
+	case JAKOTENN:
+		break;
+	case SINJU:
+		break;
+	default:
+		break;
+	}
+}
