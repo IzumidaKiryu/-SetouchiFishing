f079ee8378db8dccec07187f8a6e7cf1aedcf593
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-03-20 11:58:59 +0900

    フィッシュマネージャーのスプライトレンダー型メンバ変数の変更
    ポインタから普通の変数に変えた

--- Foruda_Kiryu/Game(kari)/turi(kari)/Buri.cpp
+++ Foruda_Kiryu/Game(kari)/turi(kari)/Buri.cpp
@@ -18,8 +18,8 @@ Buri::~Buri()
 /// </summary>
 void Buri::SetUI()
 {
-	m_ui->Init("Assets/modelData/buriUI.DDS", 150, 150);
-	m_ui->SetPivot(Vector2(0.5f, 0.5f));
-	m_ui->SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
-	m_ui->Update();
+	m_ui.Init("Assets/modelData/buriUI.DDS", 150, 150);
+	m_ui.SetPivot(Vector2(0.5f, 0.5f));
+	m_ui.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
+	m_ui.Update();
 }
