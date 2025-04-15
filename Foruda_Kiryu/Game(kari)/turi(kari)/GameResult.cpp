45e6d61ca65de644ee985d7d2088989b2a2b44a9
IzumidaKiryu <ca01244003@st.kawahara.ac.jp>
2025-04-15 15:14:20 +0900

    ResultClass

---
+++ Foruda_Kiryu/Game(kari)/turi(kari)/GameResult.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include "GameResult.h"
+
+GameResult::GameResult()
+{
+	m_fishUI[Buri].Init("Assets/modelData/buriUI.DDS", 150, 150);
+	m_fishUI[Tachiuo].Init("Assets/modelData/tatiuoUI.DDS", 150, 150);
+	m_fishUI[Tai].Init("Assets/modelData/taiUI.DDS", 150, 150);
+	m_fishUI[Hirame].Init("Assets/modelData/hirameUI.DDS", 150, 150);
+	m_fishUI[Sinzyu].Init("Assets/modelData/sinnjuUI.DDS", 150, 150);
+	m_fishUI[Zyako].Init("Assets/modelData/jakotenUI.DDS", 150, 150);
+	m_back.Init("Assets/resultSprite/yattabe2.DDS", 1920, 1080);
+}
+
+GameResult::~GameResult()
+{
+}
+
+bool GameResult::start()
+{
+	return true;
+}
+
+void GameResult::Update()
+{
+	m_fishUI[Buri].Update();
+	m_fishUI[Tachiuo].Update();
+	m_fishUI[Tai].Update();
+	m_fishUI[Hirame].Update();
+	m_fishUI[Sinzyu].Update();
+	m_fishUI[Zyako].Update();
+	m_back.Update();
+}
+
+void GameResult::Render(RenderContext& rc)
+{
+	m_back.Draw(rc);
+	m_fishUI[Buri].Draw(rc);
+	m_fishUI[Tachiuo].Draw(rc);
+	m_fishUI[Tai].Draw(rc);
+	m_fishUI[Hirame].Draw(rc);
+	m_fishUI[Sinzyu].Draw(rc);
+	m_fishUI[Zyako].Draw(rc);
+
+}
+
+void GameResult::Eaging(fishName name,Vector3 fastPos,Vector3 endPos)
+{
+	m_t += g_gameTime->GetFrameDeltaTime() / 2.0f;
+	if (m_t > 1.0f) {
+		m_t = 1.0f;
+	}
+	m_vec.Lerp(m_t, fastPos, endPos);
+	m_fishUI[name].SetPosition(m_vec);
+}
+
+
