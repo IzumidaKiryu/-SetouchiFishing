4a35916a566e917d521bf55996779bb015ca4b4c
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-04-26 13:53:28 +0900

    魚の追加

---
+++ GameTemplate/Game/Hirame.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "Hirame.h"
+
+Hirame::Hirame()
+{
+	SetUI();
+	SetBaseScore(120.0f);
+	SetScore();
+	SetTimeUntilEscape(10.0f);
+	SetInitPos(0.7f);
+}
+
+Hirame::~Hirame()
+{
+
+}
+
+/// <summary>
+/// UIÇÃê›íË
+/// </summary>
+void Hirame::SetUI()
+{
+	m_ui.Init("Assets/modelData/hirameUI.DDS", 150, 150);
+	/*m_ui.SetPivot(Vector2(0.5f, 0.5f));
+	m_ui.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
+	m_ui.Update();*/
+}
---
+++ GameTemplate/Game/Jakoten.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "Jakoten.h"
+
+Jakoten::Jakoten()
+{
+	SetUI();
+	SetBaseScore(120.0f);
+	SetScore();
+	SetTimeUntilEscape(10.0f);
+	SetInitPos(0.7f);
+}
+
+Jakoten::~Jakoten()
+{
+
+}
+
+/// <summary>
+/// UIÇÃê›íË
+/// </summary>
+void Jakoten::SetUI()
+{
+	m_ui.Init("Assets/modelData/jakotenUI.DDS", 150, 150);
+	/*m_ui.SetPivot(Vector2(0.5f, 0.5f));
+	m_ui.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
+	m_ui.Update();*/
+}
