49424328c4bc117d351fb27574937b9f6a7485cc
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-05-02 15:04:45 +0900

    背景の変更

--- GameTemplate/Game/PlayFishingBackGround.cpp
+++ GameTemplate/Game/PlayFishingBackGround.cpp
@@ -6,9 +6,11 @@ PlayFishingBackGround::PlayFishingBackGround()
 	//?R?????g?A?E?g????B
 	//PhysicsWorld::GetInstance()->EnableDrawDebugWireFrame();
 
-	modelRender.Init("Assets/modelData/stage/stage.tkm");
+	modelRender.Init("Assets/modelData/sea_kari.tkm");
+	modelRender.SetScale(Vector3(10.0f, 10.0f, 10.0f));
 	modelRender.Update();
-	physicsStaticObject.CreateFromModel(modelRender.GetModel(), modelRender.GetModel().GetWorldMatrix());
+	/*physicsStaticObject.CreateFromModel(modelRender.GetModel(), modelRender.GetModel().GetWorldMatrix());*/
+
 }
 
 PlayFishingBackGround::~PlayFishingBackGround()
@@ -19,5 +21,5 @@ PlayFishingBackGround::~PlayFishingBackGround()
 
 void PlayFishingBackGround::Render(RenderContext& rc)
 {
-	//modelRender.Draw(rc);
+	modelRender.Draw(rc);
 }
