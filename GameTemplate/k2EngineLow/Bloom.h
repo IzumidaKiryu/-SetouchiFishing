c74eed0e27a311b1bdf4fbc86e27414828a62b38
IzumidaKiryu <ca01244003@st.kawahara.ac.jp>
2025-05-08 09:52:20 +0900

    Bloomクラスの追加

--- GameTemplate/k2EngineLow/Bloom.h
+++ GameTemplate/k2EngineLow/Bloom.h
@@ -2,6 +2,21 @@
 namespace nsK2EngineLow {
 	class Bloom:public IGameObject
 	{
+	public:
+		Bloom();
+		~Bloom();
+		void Init(RenderTarget& renderTarget);
+		void Update();
+		void Render(RenderContext& rc, RenderTarget&renderTarget);
 
+		RenderTarget* mainRenderTarget;
+		RenderTarget luminanceRenderTarget;
+		
+		SpriteInitData luminanceSpriteInitData;
+		SpriteInitData finalSpriteInitData;
+		Sprite m_luminuceSprite;
+		Sprite m_finalSprite;
+		GaussianBlur gaussianBulur[4];
+	
 	};
 }
