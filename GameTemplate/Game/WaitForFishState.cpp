6fe8084f8b6257659c3bcebe40eebac4d406669c
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-05-10 10:41:22 +0900

    魚がウキに向かってくる処理の修正

--- GameTemplate/Game/WaitForFishState.cpp
+++ GameTemplate/Game/WaitForFishState.cpp
@@ -15,25 +15,27 @@ void WaitForFishState::Update()
 	ComingFish();
 	SumRodFloatPosition(Floating());
 	SetFloat();
-
+	SetCamera(GetPlayerPos() + Vector3{ 0.0f,100.0f,100.0f }, GetFishModelPos());
 }
 
 void WaitForFishState::ComingFish()
 {
-	if (m_current_fish_range_and_max_range_rate > m_current_float_range_max_range_rate) {
-		m_current_fish_range_and_max_range_rate -= 0.001;
-		if (m_current_fish_range_and_max_range_rate - m_current_float_range_max_range_rate <= 0) {
+	if (m_sum_current_fish_range_and_max_range_rate > m_sum_current_float_range_max_range_rate) {
+		m_sum_current_fish_range_and_max_range_rate -= 0.001;
+		if (m_sum_current_fish_range_and_max_range_rate - m_sum_current_float_range_max_range_rate <= 0) {
 			Success();
+			return;
 		}
 	}
 
-	if (m_current_fish_range_and_max_range_rate < m_current_float_range_max_range_rate) {
-		m_current_fish_range_and_max_range_rate += 0.001;
-		if (m_current_float_range_max_range_rate - m_current_fish_range_and_max_range_rate <= 0) {
+	if (m_sum_current_fish_range_and_max_range_rate < m_sum_current_float_range_max_range_rate) {
+		m_sum_current_fish_range_and_max_range_rate += 0.001;
+		if (m_sum_current_float_range_max_range_rate - m_sum_current_fish_range_and_max_range_rate <= 0) {
 			Success();
+			return;
 		}
 	}
 
-	SumFishModelPosition(m_current_fish_range_and_max_range_rate);
+	SumFishModelPosition(m_sum_current_fish_range_and_max_range_rate);
 	SetFish();
 }
