33e8f8c9e244a613a9646e19ecb6465b1d116178
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-05-10 10:34:03 +0900

    島の追加

--- GameTemplate/Game/PlayFishingBackGround.cpp
+++ GameTemplate/Game/PlayFishingBackGround.cpp
@@ -9,6 +9,17 @@ PlayFishingBackGround::PlayFishingBackGround()
 	modelRender.Init("Assets/modelData/sea_kari.tkm");
 	modelRender.SetScale(Vector3(10.0f, 10.0f, 10.0f));
 	modelRender.Update();
+
+	m_ship.Init("Assets/modelData/newStage/MAP.tkm");
+	m_ship.SetScale(Vector3(1.0f, 1.0f, 1.0f));
+	m_ship.Update();
+
+	
+	m_isrand.Init("Assets/modelData/stage/shima.tkm");
+	m_isrand.SetPosition(Vector3(-500.0f, 0.0f, 4000.0f));
+	m_isrand.SetScale(Vector3(10.0f, 10.0f, 10.0f));
+	m_isrand.Update();
+
 	/*physicsStaticObject.CreateFromModel(modelRender.GetModel(), modelRender.GetModel().GetWorldMatrix());*/
 
 }
@@ -22,4 +33,6 @@ PlayFishingBackGround::~PlayFishingBackGround()
 void PlayFishingBackGround::Render(RenderContext& rc)
 {
 	modelRender.Draw(rc);
+	/*m_ship.Draw(rc);*/
+	m_isrand.Draw(rc);
 }
