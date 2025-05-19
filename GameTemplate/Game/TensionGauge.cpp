6a9d4d5325512d26fdd9273940046538d656daa5
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-05-19 13:46:21 +0900

    テンションゲージクラスの魚のUIの向きが切り替わる処理

--- GameTemplate/Game/TensionGauge.cpp
+++ GameTemplate/Game/TensionGauge.cpp
@@ -6,6 +6,7 @@
 #include"RodFloatMove.h";
 #include "SceneFightFish.h"
 #include "FightFishState.h"
+#include <numbers>
 
 
 #include <random>
@@ -13,11 +14,16 @@
 
 TensionGauge::TensionGauge()
 {
+
 	srand(time(nullptr));
+	//１８０度回すための変数を初期化。
+	m_upward.SetRotationDegZ(0.0);
+	m_downward.SetRotationDegZ(180);
+
 	//m_tensionGaugeInside.Init("Assets/modelData/tensionGauge.DDS", 500, 100);
 	m_tensionGaugeInside.Init("Assets/modelData/new_sea_gauge.DDS", 100, 700);
 	m_tensionGaugeInside.SetPivot(Vector2(0.5f, 0.5f));
-	m_tensionGaugeInside.SetPosition(Vector3(650.0f,0.0f, 0.0f));
+	m_tensionGaugeInside.SetPosition(Vector3(650.0f, 0.0f, 0.0f));
 	m_tensionGaugeInside.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
 	m_tensionGaugeInside.Update();
 
@@ -31,7 +37,7 @@ TensionGauge::TensionGauge()
 	m_signs_of_Fish.SetPivot(Vector2(0.5f, 1.0f));
 	m_signs_of_Fish.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
 
-	
+
 	m_rodFloatUI.Init("Assets/modelData/rod_float.DDS", 50, 50);
 	m_rodFloatUI.SetPivot(Vector2(0.5f, 0.5f));
 	m_rodFloatUI.SetPosition(Vector3(650.0f, 0.0f, 0.0f));
@@ -46,7 +52,7 @@ TensionGauge::~TensionGauge()
 
 void TensionGauge::Update()
 {
-	m_fightFishState=FindGO<FightFishState>("fightFishState");
+	m_fightFishState = FindGO<FightFishState>("fightFishState");
 	//if (m_fightFishState != nullptr) {
 	//	Set_signs_of_Fish_UI();
 	//}
@@ -63,7 +69,7 @@ void TensionGauge::RightAndLeftManagement()
 
 void TensionGauge::SetFishUI_Position(float current_fish_range_and_max_range_rate)
 {
-	m_signs_of_Fish.SetPosition(Vector3(650.0f, m_bar_length * current_fish_range_and_max_range_rate+ m_barBottom, 0.0f));
+	m_signs_of_Fish.SetPosition(Vector3(650.0f, m_bar_length * current_fish_range_and_max_range_rate + m_barBottom, 0.0f));
 	m_signs_of_Fish.Update();
 }
 void TensionGauge::SetFloatUI_Position(float current_float_range_max_range_rate)
@@ -85,20 +91,98 @@ void TensionGauge::SetScale()
 	}
 }
 
+//void TensionGauge::Set_signs_of_Fish_UI()
+//{
+//	if (m_fightFishState->m_previous_is_fish_suited_for_upper_side != m_fightFishState->is_fish_suited_for_upper_side) {//前のフレームと状態が違う時だけ。
+//		switch (m_fightFishState->m_fishState)
+//		{
+//		case normal:
+//			switch (m_fightFishState->is_fish_suited_for_upper_side)
+//			{
+//
+//			case true:
+//				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_Facing_left.DDS", 50, 50);
+//				break;
+//				
+//			case false:
+//				/*m_signs_of_Fish.SetRotation();*/
+//				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_Facing_right.DDS", 50, 50);
+//				break;
+//
+//			default:
+//				break;
+//			}
+//			break;
+//		case announce:
+//			switch (m_fightFishState->is_fish_suited_for_upper_side)
+//			{
+//
+//			case true:
+//				m_signs_of_Fish.Init("Assets/modelData/announce_state_fish_left.DDS", 50, 100);
+//				break;
+//
+//			case false:
+//				m_signs_of_Fish.Init("Assets/modelData/announce_state_fish_right.DDS", 50, 100);
+//				break;
+//
+//			default:
+//				break;
+//			}
+//			break;
+//		case angry:
+//			switch (m_fightFishState->is_fish_suited_for_upper_side)
+//			{
+//
+//			case true:
+//				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_angry_state_left.DDS", 50, 50);
+//				break;
+//
+//			case false:
+//				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_angry_state_right.DDS", 50, 50);
+//				break;
+//
+//			default:
+//				break;
+//			}
+//			break;
+//		case exhausted:
+//			switch (m_fightFishState->is_fish_suited_for_upper_side)
+//			{
+//
+//			case true:
+//				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_Facing_left.DDS", 50, 50);
+//				break;
+//
+//			case false:
+//				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_Facing_right.DDS", 50, 50);
+//				break;
+//
+//			default:
+//				break;
+//			}
+//			break;
+//		default:
+//			break;
+//		}
+//	}
+//	m_signs_of_Fish.Update();
+//	m_fightFishState->m_previous_is_fish_suited_for_upper_side = m_fightFishState->is_fish_suited_for_upper_side;
+//}
 void TensionGauge::Set_signs_of_Fish_UI()
 {
-	if (m_fightFishState->m_previous_is_fish_suited_for_upper_side != m_fightFishState->is_fish_suited_for_upper_side) {//前のフレームと状態が違う時だけ。
+	if (m_fightFishState->m_previous_fishFacing != m_fightFishState->m_fishFacing) {//前のフレームと状態が違う時だけ。
 		switch (m_fightFishState->m_fishState)
 		{
 		case normal:
-			switch (m_fightFishState->is_fish_suited_for_upper_side)
+			switch (m_fightFishState->m_fishFacing)
 			{
 
-			case true:
+			case Upward:
 				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_Facing_left.DDS", 50, 50);
 				break;
 
-			case false:
+			case Downward:
+				/*m_signs_of_Fish.SetRotation();*/
 				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_Facing_right.DDS", 50, 50);
 				break;
 
@@ -107,14 +191,14 @@ void TensionGauge::Set_signs_of_Fish_UI()
 			}
 			break;
 		case announce:
-			switch (m_fightFishState->is_fish_suited_for_upper_side)
+			switch (m_fightFishState->m_fishFacing)
 			{
 
-			case true:
+			case Upward:
 				m_signs_of_Fish.Init("Assets/modelData/announce_state_fish_left.DDS", 50, 100);
 				break;
 
-			case false:
+			case Downward:
 				m_signs_of_Fish.Init("Assets/modelData/announce_state_fish_right.DDS", 50, 100);
 				break;
 
@@ -123,14 +207,14 @@ void TensionGauge::Set_signs_of_Fish_UI()
 			}
 			break;
 		case angry:
-			switch (m_fightFishState->is_fish_suited_for_upper_side)
+			switch (m_fightFishState->m_fishFacing)
 			{
 
-			case true:
+			case Upward:
 				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_angry_state_left.DDS", 50, 50);
 				break;
 
-			case false:
+			case Downward:
 				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_angry_state_right.DDS", 50, 50);
 				break;
 
@@ -139,14 +223,14 @@ void TensionGauge::Set_signs_of_Fish_UI()
 			}
 			break;
 		case exhausted:
-			switch (m_fightFishState->is_fish_suited_for_upper_side)
+			switch (m_fightFishState->m_fishFacing)
 			{
 
-			case true:
+			case Upward:
 				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_Facing_left.DDS", 50, 50);
 				break;
 
-			case false:
+			case Downward:
 				m_signs_of_Fish.Init("Assets/modelData/signs_of_fish_Facing_right.DDS", 50, 50);
 				break;
 
@@ -159,8 +243,21 @@ void TensionGauge::Set_signs_of_Fish_UI()
 		}
 	}
 	m_signs_of_Fish.Update();
-	m_fightFishState->m_previous_is_fish_suited_for_upper_side = m_fightFishState->is_fish_suited_for_upper_side;
+	m_fightFishState->m_previous_fishFacing = m_fightFishState->m_fishFacing;
 }
+
+void TensionGauge::SetUpwardFishUI()
+{
+	m_signs_of_Fish.SetRotation(m_upward);
+	m_signs_of_Fish.Update();
+}
+
+void TensionGauge::SetDownwardFishUI()
+{
+	m_signs_of_Fish.SetRotation(m_downward);
+	m_signs_of_Fish.Update();
+}
+
 
 void TensionGauge::Render(RenderContext& rc)
 {
