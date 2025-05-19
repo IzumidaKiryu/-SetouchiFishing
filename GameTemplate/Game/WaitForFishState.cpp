39efea3b81d6b0b7a953ce54e6f506fc3e21480e
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-05-19 13:39:49 +0900

    WaitForFishStateクラスのカメラワーク処理と処理の修正

--- GameTemplate/Game/WaitForFishState.cpp
+++ GameTemplate/Game/WaitForFishState.cpp
@@ -4,36 +4,95 @@
 WaitForFishState::WaitForFishState()
 {
 
+
 }
 
 WaitForFishState::~WaitForFishState()
 {
 }
 
+bool WaitForFishState::Start()
+{
+	PlayFishingStateBase::Start();
+
+	m_initCameraPos = m_cameraPos;
+	m_endCameraPos = m_init_floatModelPos + Vector3{ 200.0f,200.0f,200.0f };
+	m_initCameraTarget = m_cameraTarget;
+	
+
+	return true;
+}
+
+//bool WaitForFishState::Start()
+//{
+//	return true;
+//}
+
+void WaitForFishState::SetGoFishInWhich()
+{
+	//上に進むか下に進むか決める。j
+	if (m_sum_current_fish_range_and_max_range_rate > m_sum_current_float_range_max_range_rate) {
+		m_go_fish_in_which = down;
+	}
+
+	if (m_sum_current_fish_range_and_max_range_rate < m_sum_current_float_range_max_range_rate) {
+		m_go_fish_in_which = up;
+	}
+}
+
+void WaitForFishState::CameraManagement()
+{
+	m_endCameraTarget = GetFishModelPos() + ((GetFloatModelPos() - GetFishModelPos()) / 2) + Vector3{0.0f,100.0f,0.0f};
+
+	m_cameraPos_t += 0.02;
+	m_cameraTarget_t += 0.2;
+
+	if (m_cameraPos_t > 1) {
+		m_cameraPos_t = 1;
+	}
+	if (m_cameraTarget_t > 1) {
+		m_cameraTarget_t = 1;
+	}
+	m_cameraPos.Lerp(m_cameraPos_t, m_initCameraPos, m_endCameraPos);
+	m_cameraTarget.Lerp(m_cameraTarget_t, m_initCameraTarget, m_endCameraTarget);
+
+	SetCamera(m_cameraPos,m_cameraTarget);
+}
+
 void WaitForFishState::Update()
 {
+	//カメラを管理する。
+	CameraManagement();
+	SetGoFishInWhich();
 	ComingFish();
 	SumRodFloatPosition(Floating());
 	SetFloat();
-	SetCamera(GetPlayerPos() + Vector3{ 0.0f,100.0f,100.0f }, GetFishModelPos());
 }
 
 void WaitForFishState::ComingFish()
 {
-	if (m_sum_current_fish_range_and_max_range_rate > m_sum_current_float_range_max_range_rate) {
-		m_sum_current_fish_range_and_max_range_rate -= 0.001;
-		if (m_sum_current_fish_range_and_max_range_rate - m_sum_current_float_range_max_range_rate <= 0) {
-			Success();
-			return;
-		}
-	}
 
-	if (m_sum_current_fish_range_and_max_range_rate < m_sum_current_float_range_max_range_rate) {
+
+	switch (m_go_fish_in_which)
+	{
+	case WaitForFishState::up:
+		SetFishUpward();
 		m_sum_current_fish_range_and_max_range_rate += 0.001;
-		if (m_sum_current_float_range_max_range_rate - m_sum_current_fish_range_and_max_range_rate <= 0) {
+		if (m_sum_current_fish_range_and_max_range_rate - m_sum_current_float_range_max_range_rate >= 0) {
 			Success();
 			return;
 		}
+		break;
+	case WaitForFishState::down:
+		SetFishDownward();
+		m_sum_current_fish_range_and_max_range_rate -= 0.001;
+		if (-m_sum_current_float_range_max_range_rate + m_sum_current_fish_range_and_max_range_rate <= 0) {
+			Success();
+			return;
+		}
+		break;
+	default:
+		break;
 	}
 
 	SumFishModelPosition(m_sum_current_fish_range_and_max_range_rate);
--- GameTemplate/Game/WaitForFishState.h
+++ GameTemplate/Game/WaitForFishState.h
@@ -4,11 +4,21 @@
 class WaitForFishState :public PlayFishingStateBase
 {
 public:
+	enum go_fish_in_which {
+		up,
+		down,
+	};
+	bool Start();
 
 	WaitForFishState();
 	~WaitForFishState();
+	/*bool Start();*/
+	void SetGoFishInWhich();
+	void CameraManagement()override;
 	void Update();
 
 	void ComingFish();
+	go_fish_in_which m_go_fish_in_which;//どっちの方向に進むか。
+
 };
 
