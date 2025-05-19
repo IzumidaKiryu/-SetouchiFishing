3152979989956bf09586727acc2b70444e2663d4
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-05-19 14:02:22 +0900

    魚にヒットしたらUIが出る処理

--- GameTemplate/Game/HitUIState.cpp
+++ GameTemplate/Game/HitUIState.cpp
@@ -1,3 +1,111 @@
 #include "stdafx.h"
 #include "HitUIState.h"
 
+HitUIState::HitUIState()
+{
+	
+
+	m_hitUI.Init("Assets/modelData/Hit!_UI.DDS", 500, 200);
+	m_hitUI.SetPivot(Vector2(0.5f, 0.5f));
+	m_hitUI.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
+	m_hitUI.Update();
+
+	m_hitUI_Bar.Init("Assets/modelData/Hit!_UI_Bar.DDS", 1500, 300);
+	m_hitUI_Bar.SetPivot(Vector2(0.5f, 0.5f));
+	m_hitUI_Bar.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
+	m_hitUI_Bar.Update();
+
+	
+}
+
+void HitUIState::Update()
+{
+	UIMoveStateManager();
+	SetPosition();
+	SetCamera(m_cameraPos, m_cameraTarget);
+	SetFish();
+	SetFloat();
+	
+}
+
+bool HitUIState::Start()
+{
+	PlayFishingStateBase::Start();
+	m_initCameraPos = { m_cameraPos};
+	m_endCameraPos = { GetPlayerPos() + Vector3 {0.0f,100.0f,100.0f}};
+	m_initCameraTarget = { m_cameraTarget };
+	m_endCameraTarget = { m_init_fishModelPos };
+	return true;
+}
+
+void HitUIState::UIMoveStateManager()
+{
+	switch (m_uiMoveState)
+	{
+	case HitUIState::in:
+		In();
+		break;
+	case HitUIState::wait:
+		Wait();
+		break;
+	case HitUIState::out:
+		Out();
+		CameraManagement();
+		break;
+	default:
+		break;
+	}
+	
+}
+
+void HitUIState::SetPosition()
+{
+	m_hitUI.SetPosition(m_hitUIPos);
+	m_hitUI.Update();
+	m_hitUI_Bar.SetPosition(m_hitUIBarPos);
+	m_hitUI_Bar.Update();
+}
+
+void HitUIState::In()
+{
+	m_t += 0.05;
+
+	
+	m_hitUIPos.Lerp(m_t*m_t, m_initHitUIPos_in, m_endHitUIPos_in);
+	m_hitUIBarPos.Lerp(m_t*m_t, m_initHitUIBarPos_in, m_endHitUIBarPos_in);
+
+	if (m_t*m_t > 1) {
+		m_uiMoveState = wait;
+		m_t = 0;
+	}
+}
+
+void HitUIState::Wait()
+{
+	m_t += 0.05;
+	if (m_t > 1) {
+		m_uiMoveState = out;
+		m_t = 0;
+	}
+}
+
+void HitUIState::Out()
+{
+	m_t += 0.05;
+
+	if ((m_t * m_t)<=1) {
+		m_hitUIPos.Lerp(m_t * m_t, m_initHitUIPos_out, m_endHitUIPos_out);
+		m_hitUIBarPos.Lerp(m_t * m_t, m_initHitUIBarPos_out, m_endHitUIBarPos_out);
+	}
+
+	if (m_t > 1) {
+		Success();
+	}
+}
+
+
+void HitUIState::Render(RenderContext& rc)
+{
+	m_hitUI_Bar.Draw(rc);
+	m_hitUI.Draw(rc);
+}
--- GameTemplate/Game/HitUIState.h
+++ GameTemplate/Game/HitUIState.h
@@ -4,5 +4,44 @@
 class HitUIState :public PlayFishingStateBase
 {
 
+public:
+	enum UIMoveState {
+		in,
+		wait,
+		out,
+	};
+
+	HitUIState();
+	void Update();
+	bool Start();
+	void UIMoveStateManager();
+	void SetPosition();
+	void In();
+	void Wait();
+	void Out();
+	void Render(RenderContext& rc);
+
+
+	
+	float m_t;
+	Vector3 m_hitUIPos;
+	Vector3 m_hitUIBarPos;
+
+	Vector3 m_initHitUIPos_in = { 1500.0f,0.0f,0.0f };
+	Vector3 m_initHitUIBarPos_in = { 1500.0f,0.0f,0.0f };
+	Vector3 m_endHitUIPos_in = { 0.0f,0.0f,0.0f };
+	Vector3 m_endHitUIBarPos_in{ 0.0f,0.0f,0.0f };
+
+
+	Vector3 m_initHitUIPos_out = { 0.0f,0.0f,0.0f };
+	Vector3 m_initHitUIBarPos_out = { 0.0f,0.0f,0.0f };
+	Vector3 m_endHitUIPos_out = { -1500.0f,0.0f,0.0f };
+	Vector3 m_endHitUIBarPos_out = { -1500.0f,0.0f,0.0f };
+
+	
+	SpriteRender m_hitUI;
+	SpriteRender m_hitUI_Bar;
+
+	UIMoveState m_uiMoveState;
 };
 
