95b56b48fc5d552bd89ce62b47245ca3aa6527bd
TakahasiHiroyuki <167502412+takahasihiroyuki@users.noreply.github.com>
2025-05-28 13:38:12 +0900

    Takahashi/count down (#89)
    Co-authored-by: IzumidaKiryu <167502113+IzumidaKiryu@users.noreply.github.com>

---
+++ GameTemplate/Game/GameStartCountdown.h
@@ -0,0 +1,125 @@
+#pragma once
+#include"GameStateBase.h"
+#include< memory >
+
+
+enum class CountdownState
+{
+	None = -1,
+	Three,
+	Two,
+	One,
+	Start,
+	NextScene
+};
+
+class PositionSelection;
+class FishSlot;
+class BackGround;
+class GameCamera; 
+class GameStartCountdown : public GameStateBase
+{
+public:
+
+	GameStartCountdown();
+	~GameStartCountdown();
+
+	bool Start();
+	void FindGameObjects();
+
+	bool ShouldChangeState(); // 状態を変更すべきかどうかを決定する純粋仮想関数
+	void OnUpdate();//毎フレームの処理はここに書く。
+	void OnEnter() override;
+	void OnExit() override;
+
+private:
+
+
+
+
+	void SetInitCameraToShipDistance();
+	CountdownState ConvertTimeToCountdownState(int time);
+	void UpdateCountdownState();
+
+	void CountStart();
+
+	std::string ToString(CountdownState count);
+
+	void InitUI();
+
+	void SetUItexture();
+
+	void UpdateUI();
+
+	void SetUIPivot(Vector2 pivot);
+
+	void SetUIPosition(Vector3 pos);
+
+
+	void UpdateUIScale();
+	void SetUIScale(Vector3 scale);
+
+
+	void SetCamera();
+	void SetCameraPosition(Vector3 pos);
+	Vector3 UpdateCameraPostion();
+	float UpdateCameraToShipDistance();
+
+	
+	void UpdateCamera();
+
+	float Lerp(float t,float init,float end);
+	Vector2 Subtract(const Vector2& a, const Vector2& b);
+
+	float  GetUIRatios();
+	float GetCameraRatios();
+	float GetCameraToShipDistance();
+
+	void SetCameraTarget();
+	void SetInitCameraPos();
+	void SetEndCameraPos();
+	void SetInitCameraRotation();
+
+
+	void Render(RenderContext& rc);
+
+	void NextScene();
+
+	bool isCountStart=true;
+
+	CountdownState m_countdownState = CountdownState::Three;
+
+	Vector2 m_UIPivot = { 0.5f,0.5f};
+	Vector3 m_UIPos = { 0.0f,0.0f,0.0f };
+
+
+	const Vector3 m_initUIScale = Vector3::One*0.5;
+	const Vector3 m_endUIScale = Vector3::One*5;
+	Vector3 m_UIScale ;
+
+
+	SpriteRender m_countdownUI[4];
+
+
+	Vector3 m_cameraPos;
+	Vector3 m_initCameraPos;
+	Vector3 m_endCameraPos;
+
+	Vector3 m_cameraTarget;
+
+
+	float m_initCameraToShipDistance=0;
+	float m_endCameraToShipDistance = 0;
+	float m_CameraToShipDistance=0;
+
+
+	Vector3 m_endCameraTarget;
+
+	Stopwatch m_stopwatch;
+
+	PositionSelection* m_positionSelection;
+	FishSlot* m_fishSlot;
+	BackGround* m_backGround;
+	GameCamera* m_camera;
+};
+
