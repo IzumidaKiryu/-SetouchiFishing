501b8f860d529b375073ed3f42eab161df0954c8
IzumidaKiryu <ca01244003@st.kawahara.ac.jp>
2025-06-04 14:31:46 +0900

    FontRenderが出力されないエラーの修正

--- GameTemplate/k2EngineLow/FontRender.cpp
+++ GameTemplate/k2EngineLow/FontRender.cpp
@@ -1,19 +1,12 @@
 #include "k2EngineLowPreCompile.h"
 #include "FontRender.h"
 
-void nsK2EngineLow::FontRender::OnDraw(RenderContext& rc)
-{
-	if (m_text == nullptr)
-	{
-		return;
-	}
-	m_font.Begin(rc);
-	m_font.Draw(m_text, Vector2(m_position.x, m_position.y), m_color, m_rotation, m_scale, m_pivot);
-	m_font.End(rc);
-}
 
 void nsK2EngineLow::FontRender::Draw(RenderContext& rc)
 {
+	if (m_text == nullptr) {
+		return;
+	}
 	g_renderingEngine->AddRenderObject(this);
 
 }
