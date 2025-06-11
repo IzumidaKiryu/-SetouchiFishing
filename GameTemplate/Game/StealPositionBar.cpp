3485604e9f561bdb5d89f3f3dd2413814deed7b6
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-06-11 14:18:49 +0900

    StealPositionBarのUIを実装

--- GameTemplate/Game/StealPositionBar.cpp
+++ GameTemplate/Game/StealPositionBar.cpp
@@ -2,6 +2,7 @@
 #include "StealPositionBar.h"
 #include "PositionSelection.h"
 #include "Enemy.h"
+#include "BuffManager.h"
 
 StealPositionBar::StealPositionBar()
 {
@@ -13,27 +14,53 @@ void StealPositionBar::DisplayUI(RenderContext& rc)
 	m_stealPositionBarOutsideUI.Draw(rc);
 	m_stealPositionGaugeUI.Draw(rc);
 	m_stealPositionBarInsideUI.Draw(rc);
-		
+	if (CanUseGauge()) {
+		m_takeOver.Draw(rc);
+	}
+	else {
+		m_takeOver_gray.Draw(rc);
+	}
+	m_stockCountUI.Draw(rc);
 
+	m_clossUI.Draw(rc);
 }
 
 bool StealPositionBar::Init()
 {
-	m_stealPositionBarOutsideUI.Init("Assets/modelData/landscape_gauge_inside.DDS", 500, 100);
+	m_stealPositionBarOutsideUI.Init("Assets/modelData/landscape_gauge_inside.DDS", 510, 110);
 	m_stealPositionBarOutsideUI.SetPivot(Vector2(0.5f, 0.5f));
-	m_stealPositionBarOutsideUI.SetPosition(Vector3{ 500.0f, 390.0f, 0.0f });
+	m_stealPositionBarOutsideUI.SetPosition(Vector3{ 400.0f, 390.0f, 0.0f });
 	m_stealPositionBarOutsideUI.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
 	m_stealPositionBarOutsideUI.Update();
 
-	m_stealPositionBarInsideUI.Init("Assets/modelData/landscape_gauge_outer.DDS", 500, 100);
+	m_stealPositionBarInsideUI.Init("Assets/modelData/landscape_gauge_outer.DDS", 510, 110);
 	m_stealPositionBarInsideUI.SetPivot(Vector2(0.5f, 0.5f));
-	m_stealPositionBarInsideUI.SetPosition(Vector3{ 500.0f, 390.0f, 0.0f });
+	m_stealPositionBarInsideUI.SetPosition(Vector3{ 400.0f, 390.0f, 0.0f });
 	m_stealPositionBarInsideUI.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
 	m_stealPositionBarInsideUI.Update();
 
-	m_stealPositionGaugeUI.Init("Assets/modelData/cast_successful.DDS", 500.0f, 100);
+	m_stealPositionGaugeUI.Init("Assets/modelData/takeOver/takeOverGauge.DDS", 500.0f, 100);
 	m_stealPositionGaugeUI.SetPivot(Vector2(0.0f, 0.5f));
-	m_stealPositionGaugeUI.SetPosition(Vector3{ 500.0f - 240.0f, 390.0f, 0.0f });
+	m_stealPositionGaugeUI.SetPosition(Vector3{ 160.0f, 390.0f, 0.0f });
+
+	m_takeOver_gray.Init("Assets/modelData/takeOver/takeOver_gray.DDS", 200.0f, 100);
+	m_takeOver_gray.SetPivot(Vector2(0.0f, 0.5f));
+	m_takeOver_gray.SetPosition(Vector3{ -50.0f, 390.0f, 0.0f });
+	m_takeOver_gray.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
+	m_takeOver_gray.Update();
+
+	m_takeOver.Init("Assets/modelData/takeOver/takeOver.DDS", 200.0f, 100);
+	m_takeOver.SetPivot(Vector2(0.0f, 0.5f));
+	m_takeOver.SetPosition(Vector3{ -50.0f, 390.0f, 0.0f });
+	m_takeOver.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
+	m_takeOver.Update();
+
+	m_clossUI.Init("Assets/modelData/takeOver/Cross.DDS", 30.0f, 30.0f);
+	m_clossUI.SetPivot(Vector2(0.0f, 0.5f));
+	m_clossUI.SetPosition(Vector3{ 650.0f, 370.0f, 0.0f });
+	m_clossUI.SetScale(Vector3{ 1.0f, 1.0f, 1.0f });
+	m_clossUI.Update();
+
 
 	return true;
 }
@@ -42,13 +69,14 @@ bool StealPositionBar::Start()
 {
 	m_positionSelection = FindGO<PositionSelection>("positionSelection");
 	m_enemy = FindGO<Enemy>("enemy");
+	m_buffManager = FindGO<BuffManager>("buffManager");
 	return true;
 }
 
 void StealPositionBar::Update()
 {
 	//ゲージを更新
-	UpdateStealGauge(m_gaugeDeltaPerFrame);
+	UpdateStealGauge();
 	//バーの長さを決める。
 	SetBarLength();
 
@@ -71,9 +99,10 @@ void StealPositionBar::SetBarLength()
 	m_stealPositionGaugeUI.Update();
 }
 
-void StealPositionBar::UpdateStealGauge(float addPoint)
+void StealPositionBar::UpdateStealGauge()
 {
-	m_stealPoint += addPoint;
+	m_stealPoint += m_gaugeDeltaPerFrame+m_buffManager->GetTotalStealBoostBuff();
+	SetStockCounUI();
 }
 
 void StealPositionBar::UseStealGauge()
@@ -128,6 +157,21 @@ void StealPositionBar::SetIsStealLockActive(bool isActive, Area area)
 bool StealPositionBar::GetIsStealLockActive(Area area)
 {
 	return m_isStealLockActive[static_cast<int>(area)];
+}
+
+void StealPositionBar::SetStockCounUI()
+{
+		wchar_t wcsbuf[256];
+	swprintf_s(wcsbuf, 256, L"%d", int(m_stockCount));
+
+	//表示するテキストを設定。
+	m_stockCountUI.SetText(wcsbuf);
+	//フォントの位置を設定。
+	m_stockCountUI.SetPosition(Vector3(800.0f, 530.0f, 0.0f));
+	//フォントの大きさを設定。
+	m_stockCountUI.SetScale(3.0f);
+	//フォントの色を設定。
+	m_stockCountUI.SetColor({ 1.0f,0.3f,0.0f,1.0f });
 }
 
 void StealPositionBar::InitisStealLockActive()
