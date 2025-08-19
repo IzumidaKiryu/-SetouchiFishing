ad1fdcb7d037284a5fa916a642bd366aed4cfe5d
takahasihiroyuki <CA01244014@st.kawahara.ac.jp>
2025-08-19 16:49:45 +0900

    エフェクトマネージャーの追加

---
+++ GameTemplate/Game/EffectManager.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "EffectManager.h"
+
+void EffectManager::Update()
+{
+}
+
+void EffectManager::UpdateEffects()
+{
+}
+
+bool EffectManager::Start()
+{
+
+	return true;
+}
+
